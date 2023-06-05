0e5c0fea754a5961b211ad2ebc68a11995809def
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-06 01:34:00 +0200

    circle in linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * check_cycle - checks if and linke list is circular or not.
+ * @list: point to the head node of the list
+ * Return: 0 if not a circular list or 1 if circular
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current_node = list;
+
+	while (current_node != NULL)
+	{
+		if (current_node->next == list)
+			return (1);
+		current_node = current_node->next;
+	}
+	return (0);
+}
