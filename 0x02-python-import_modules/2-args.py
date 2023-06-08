071013180defd1ccd02f671fe82e00b04ba9bee0
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-09 01:15:04 +0200

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-import sys
 
-arg_arr = sys.argv
-size = len(arg_arr)
+if __name__ == "__main_":
+    """takes command line arguments"""
+    import sys
 
-if size == 1:
-    print("{} arguments.".format(size - 1))
-elif size == 2:
-    print("{} argument:".format(size - 1))
-    print("{}: {}".format(size - 1, arg_arr[size - 1]))
-else:
-    print("{} arguments:".format(size - 1))
-    for n in range(1, size):
-        print("{}: {}".format(n, arg_arr[n]))
+    arg_arr = sys.argv
+    size = len(arg_arr)
+
+    if size == 1:
+        print("{} arguments.".format(size - 1))
+    elif size == 2:
+        print("{} argument:".format(size - 1))
+        print("{}: {}".format(size - 1, arg_arr[size - 1]))
+    else:
+        print("{} arguments:".format(size - 1))
+        for n in range(1, size):
+            print("{}: {}".format(n, arg_arr[n]))
