5f9080696bdb7e1477a38cbe59b965d842d12ec2
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-12 20:38:00 +0200

    bug fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -11,12 +11,13 @@ def new_in_list(my_list, idx, element):
         element: item to add at position idx
     Returns: modified copy of the original list or unmodified copy.
     """
-    copy_list = [n for n in my_list]
+    if isinstance(my_list, list):
+        copy_list = [n for n in my_list]
 
-    if idx < 0:
-        return copy_list
-    elif idx > len(my_list):
-        return copy_list
-    else:
-        copy_list[idx] = element
-        return copy_list
+        if idx < 0:
+            return copy_list
+        elif idx > len(my_list):
+            return copy_list
+        else:
+            copy_list[idx] = element
+            return copy_list
