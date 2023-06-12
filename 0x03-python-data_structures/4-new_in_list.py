e3ea876595eaf837531eafd105f5e58ff37134ce
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-12 20:01:45 +0200

    copy the list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position
+       without modifying the original list.
+
+    args:
+        my_list:  original list to make a copy from.
+        idx: index position to modify a value in a copy of the list.
+        element: item to add at position idx
+    Returns: modified copy of the original list or unmodified copy.
+    """
+    copy_list = [n for n in my_list]
+
+    if idx < 0 or idx > len(my_list):
+        return copy_list
+    else:
+        copy_list[idx] = element
+        return copy_list
