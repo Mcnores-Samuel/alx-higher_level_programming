1175abe29689a9c588ac49d418579a2878980a27
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-12 20:28:36 +0200

    new string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    """Removes all characters c and C from a string.
+
+    args:
+        my_string: a string literal to removes c or C from.
+    Returns: a new string without c or C characters.
+    """
+    result_str = ""
+    for char in my_string:
+        if char != "c" or char != "C":
+            result_str += char
+
+    return result_str
