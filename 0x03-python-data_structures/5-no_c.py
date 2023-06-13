b43a6b40f415cc0148de1d564e71507a1b17822e
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-13 14:19:48 +0200

    remove c characters

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,11 +8,11 @@ def no_c(my_string):
         my_string: a string literal to removes c or C from.
     Returns: a new string without c or C characters.
     """
+    if my_string == "":
+        return
     if my_string:
         result_str = ""
         for char in my_string:
             if char != "c" and char != "C":
                 result_str += char
         return result_str
-    else:
-        return
