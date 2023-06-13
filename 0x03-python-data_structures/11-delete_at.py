997ba25a3e53a7601cee61de25d884211d5048a9
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-13 03:20:14 +0200

    delete a value in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    """Deletes the item at a specific position in a list.
+
+    args:
+        my_list: a list to delete a value from.
+        idx: index position at which a value is to be deleted.
+    Returns: a new list without the deleted value
+    """
+    if isinstance(my_list, list):
+        if my_list:
+            if idx < 0 or idx > len(my_list) - 1:
+                return my_list
+            else:
+                del my_list[idx]
+                return my_list
