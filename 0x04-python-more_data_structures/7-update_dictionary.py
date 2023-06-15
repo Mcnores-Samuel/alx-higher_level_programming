e9dffa57f24aecb234f4517b07abb23a0bc63a23
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-15 03:33:54 +0200

    replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds key/value in a dictionary.
+
+    args:
+        a_dictionary: a dictionary to manipulate or update_dictionary
+        key: a key to check if it exist or create it.
+        value: a value to the key
+    Returns: an updated dictionary
+    """
+    if a_dictionary and key and value:
+        if isinstance(a_dictionary, dict):
+            if key in a_dictionary:
+                a_dictionary[key] = value
+            else:
+                a_dictionary[key] = value
+    return a_dictionary
