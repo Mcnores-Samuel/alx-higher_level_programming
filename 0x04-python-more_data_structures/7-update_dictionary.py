638548366d2704c373f362425c451924c7c0d710
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-15 03:35:19 +0200

    replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -16,4 +16,6 @@ def update_dictionary(a_dictionary, key, value):
                 a_dictionary[key] = value
             else:
                 a_dictionary[key] = value
-    return a_dictionary
+        return a_dictionary
+    else:
+        return {}
