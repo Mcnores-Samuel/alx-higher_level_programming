0507b022d574a402a8435ed5fc62aace5cac2c1d
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-27 11:13:23 +0200

    raises a name exception with a message.

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """Raises a name exception with a message.
+
+    args:
+        message: The message to be printed when the exception is raised.
+    """
+    raise NameError(message)
