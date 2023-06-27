1e9937206581bae875b6398632498b35a440fe10
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-27 04:00:43 +0200

    python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,6 +14,7 @@ def safe_print_list(my_list=[], x=0):
             for n in range(x):
                 i += 1
                 print("{}".format(my_list[n]), end="")
+            print()
     except IndexError:
         pass
     return (i)
