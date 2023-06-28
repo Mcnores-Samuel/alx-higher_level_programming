554befb569a86db371926171829d9fac0c739121
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-28 02:48:51 +0200

    defines size attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,13 +14,8 @@ class Square:
         args:
             size: integer.
         """
-        try:
-            self.__size = size
-            if self.__size is not isinstance(self.__size, int):
-                raise TypeError
-            if self.__size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
