bb00361684a7984087051ac0e4ec7db6df2d74d5
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-28 03:09:35 +0200

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""This module defines a class with a size attribute.
+
+size is a private instance attribute.
+"""
+
+
+class Square:
+    """Defines a size private instance attribute"""
+    def __init__(self, size=0):
+        """Size defined with type or value verification.
+
+        args:
+            size: integer.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrives a size value"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size * self.__size)
