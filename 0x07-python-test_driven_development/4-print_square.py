4acbec5f3203c3043c84d90375b87a1952493c07
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-05 02:37:57 +0200

    Test driven development

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""This module contains one function print_square(size)
+
+print_square(size) Prints a square of length (size) with
+the character #
+for example:
+>>> print_square(4)
+####
+####
+####
+####
+>>> print_square(10)
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+"""
+
+
+def print_square(size):
+    """Prints a square with the character #
+
+    args:
+        size: the size length of the square
+    Returns: None
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for n in range(size):
+        print("#" * size)
