61db7ff00a2839e81f0a4ac65835c4c0dc977ea3
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-11 23:50:13 +0200

    read, write and appends text to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -15,6 +15,6 @@ def append_write(filename="", text=""):
     Returns: number of characters written.
     """
     written = 0
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         written = file.write(text)
     return (written)
