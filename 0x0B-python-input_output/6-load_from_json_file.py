f694f157b830c26403e43b8eee6b36593eab212f
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-12 01:05:58 +0200

    Deserialization

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This module defines a function load_from_json_file(filename)
+
+load_from_json_file(filename) creates an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an Object from a “JSON file”
+
+    args:
+        filename: JSON file to create an object from.
+    Returns: the object created or Deserialized
+    """
+    obj = ""
+    with open(filename, "r") as file:
+        obj = json.load(file)
+    return(obj)
