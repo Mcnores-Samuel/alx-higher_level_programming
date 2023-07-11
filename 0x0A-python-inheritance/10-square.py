16a4ddfa91887dc3414ade0a34e686b5fcd83e29
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-11 02:33:12 +0200

    Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,18 +3,23 @@
 """This module defines a Square class and also inherits
 from Rectangle class in the 9-rectangle module.
 """
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """defines a Square class and defines a constractor
+    """
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
+        """Initializes the Square class
+        args:
+            size: a positive integer.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+
+if __name__ == "__main__":
+    s = Square(13)
+    print(s)
+    print(s.area())
