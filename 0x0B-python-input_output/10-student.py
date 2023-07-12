da933f84d314e5c6aa48a868801f10fb709d86bc
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-12 02:52:23 +0200

    returns the dictionary description with simple data structure

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This module defines a Student class"""
+
+
+class Student:
+    """defines a student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes Student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=[]):
+        """retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(type(item) == str
+                                           for item in attrs):
+            return ({key: getattr(self, key) for
+                     key in attrs if hasattr(self, key)})
+        return (self.__dict__)
