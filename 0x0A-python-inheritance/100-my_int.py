c4fbda42cc2145435d890d65c41c8a9756d18323
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-12 20:30:55 +0200

    fixed import bug

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,13 +8,9 @@ from the int class.
 class MyInt(int):
     """Defines MyInt class which inverts the == and != operators"""
     def __ne__(self, value):
-        """Return the opposite if obj != value return True in place
-        False
-        """
+        """Return the opposite if obj != value return True in place False"""
         return (super().__eq__(value))
 
     def __eq__(self, value):
-        """Return the opposite if obj == value return False in place
-        True
-        """
+        """Return the opposite if obj == value return False in place True"""
         return (not super().__eq__(value))
