11ccd73ae31288a9350d786adbfcbb1db7c00e01
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-12 02:15:02 +0200

    adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,16 +7,11 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-try:
-    data_list = load_from_json_file("add_item.json")
-    if len(data_list) != 0:
-        data = sys.argv
-        if len(data) > 1:
-            for item in range(1, len(data)):
-                try:
-                    data_list.append(data[item])
-                except IndentationError:
-                    break
-        save_to_json_file(data_list, "add_item.json")
-except FileNotFoundError:
-    save_to_json_file([], "add_item.json")
+if __name__ == "__main__":
+    try:
+        data_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data_list = []
+    data = sys.argv[1:]
+    data_list += data
+    save_to_json_file(data_list, "add_item.json")
