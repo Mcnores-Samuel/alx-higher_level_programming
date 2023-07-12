7424aef165f90889ec5aa3296ee64090506cdb71
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-12 11:44:35 +0200

    reloads from json file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""This module defines a Student class"""
+
+
+class Student:
+    """defines a student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes Student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=[]):
+        """retrieves a dictionary representation of a Student instance
+        args:
+           attrs: is a list of strings, only attribute names contained
+           in this list must be retrieved.
+        """
+        if type(attrs) == list and all(type(item) == str
+                                       for item in attrs):
+            return ({key: getattr(self, key) for
+                     key in attrs if hasattr(self, key)})
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+
+        args:
+            json: a dictionary
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
