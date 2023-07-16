a72bb49af7da3474e7f7e695cf5dd19c1b76bc02
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-16 12:22:21 +0200

    update type checking

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -25,14 +25,14 @@ def add_integer(a, b=98):
         b: second integer
     Returns: an integer: the addition of a and b
     """
-    if a is None or (type(a) is not int and type(a) is not float):
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+    if not a or type(a) != int:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    elif type(b) != int:
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
-    return int(a) + int(b)
+    return (a + b)
 
 
 if __name__ == "__main__":
