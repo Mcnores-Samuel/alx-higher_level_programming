26d3f3fb3c35747e29a45729bc82da18322f29c9
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-08-19 09:37:09 +0200

    lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,10 @@
-SELECT  show.title FROM tv_shows AS show
-INNER JOIN tv_show_genres ON show.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genres_id
+-- Lists all Comedy shows in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+SELECT  tv_shows.title FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY show.title;
+ORDER BY tv_shows.title;
