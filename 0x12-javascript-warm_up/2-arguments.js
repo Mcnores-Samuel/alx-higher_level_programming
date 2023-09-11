adcabf1bcfd552d209613ca0bf9e51384a14d5d1
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-11 22:28:53 +0200

    command line arguements

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const cmdLineArgs = process.argv.slice(2);
+const numOfArgs = cmdLineArgs.length;
+if (numOfArgs === 0) {
+	console.log('No argument');
+} else if (numOfArgs === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+};
