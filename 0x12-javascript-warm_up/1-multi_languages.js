a6323a45e41b93d75d42d4c89c44d40be8485732
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-11 22:06:27 +0200

    prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const comps = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let pos = 0; pos < comps.length; pos++) console.log(comps[pos])
+const comps = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let pos = 0; pos < comps.length; pos++) console.log(comps[pos]);
