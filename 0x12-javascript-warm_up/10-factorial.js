44a32b6a3781b74393c667f43b7098cc3e5cdc77
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-12 00:06:02 +0200

    computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * factorial -  computes and prints a factorial
+ * @num - integer to factorize
+ * @returns a factorial of a number
+ */
+const factorial = (num) => {
+  if (!num || num === 1) {
+    return (1);
+  } else {
+    return factorial(num - 1) * num;
+  }
+};
+
+const args = process.argv.slice(2);
+const num = Number(args[0]);
+console.log(factorial(num));
