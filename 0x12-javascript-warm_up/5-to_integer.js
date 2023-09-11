d1e316e17f927ef22cdeb476bc662e383eae6591
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-11 23:19:28 +0200

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 const args = process.argv.slice(2);
-num = Number(args[0])
-console.log( num ? 'My number: ' + num : 'Not a number');
+num = Number(args[0]);
+console.log(num ? 'My number: ' + num : 'Not a number');
