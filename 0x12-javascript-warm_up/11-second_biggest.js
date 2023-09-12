1b6e3be58eccb4035bdd7175ea2c7bceaf97fb63
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-12 02:04:01 +0200

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args.length === 0 || args.length === 1) {
+    console.log(0)
+} else {
+    let arrOfNums = args.map((elem) => Number(elem)).sort((a, b) => a - b)
+    const secondMax = arrOfNums[arrOfNums.length - 2]
+    console.log(secondMax)
+}
