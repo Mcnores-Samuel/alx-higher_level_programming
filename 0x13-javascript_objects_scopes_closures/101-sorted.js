f37add877a670c07c8e9d415bb1c31ba2d486f24
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-14 04:38:42 +0200

    imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-
 const array = Object.entries(dict);
 const newdict = {};
 
 for (let n = 0; n < array.length; n++) {
   const key = String(array[n][1]);
   const val = array[n][0];
-  if (newdict.hasOwnProperty(key)) {
+  if (newdict[key]) {
     const arr = newdict[key];
     arr.push(val);
   } else {
