b067d731e1212df29eb013bb5e288eec04cdb243
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-17 10:52:22 +0200

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module provides a script for reading data from mysql database
+
+Lists all states from the database hbtn_0e_0_usa
+
+Usage:
+    Take 3 arguments: mysql username, mysql password and database name
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+
+    try:
+        database = MySQLdb.connect(
+            host='localhost', port=3306,
+            user=args[1], password=args[2],
+            database=args[3]
+        )
+
+        db_cursor = database.cursor()
+        db_cursor.execute("""
+                          SELECT * FROM states ORDER BY states.id ASC
+                          """)
+        states = db_cursor.fetchall()
+        for state in states:
+            print(state)
+    except IndexError:
+        pass
