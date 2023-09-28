0098f317ce10e24dc9980accf2ac115e63ebf21a
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-28 23:31:38 +0200

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sLI "$1" | grep -i Content-Length | awk '{print $2}'
+#!/bin/bash
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
