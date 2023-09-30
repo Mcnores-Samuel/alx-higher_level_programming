f6c3f49c9fcbc9d47df093db34329a69f0eca842
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-30 12:28:55 +0200

    takes in a letter and sends a POST request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
             print("[{}] {}".format(response.get("id"), response.get("name")))
         else:
             print("No result")
-    except (requests.exceptions.JSONDecodeError, ValueError):
+    except ValueError:
         print("Not a valid JSON")
