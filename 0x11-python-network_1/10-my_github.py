6789b9fd2232714434c29fce79da16673f90068a
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-01 02:32:02 +0200

    takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user/"
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    print(response.json().get('id'))
