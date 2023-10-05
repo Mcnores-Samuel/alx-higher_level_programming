8a66c4050aa0fe3f1bb9388a3e6f3bf121bf01ec
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-05 04:06:53 +0200

    sends a request to a URL passed as an argument, and displays only the status code of the response.

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+# sends a JSON POST request to a URL passed as the first argument.
 curl -sX POST -d "$2" "$1"
