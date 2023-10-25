5997fc5067229d27de08bc7228fb8d88eb366ee7
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-25 02:28:00 +0200

    display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-/*display the status code of a GET request */
+/* display the status code of a GET request */
 const url = process.argv.slice(2)[0];
-const request = require("request");
+const request = require('request');
 request.get(url, (error, response) => {
-    console.log('code: ', response.statusCode)
-})
+  console.log('code: ', response.statusCode);
+});
